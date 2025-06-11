edb574c783716f5d35e0f47356c28bcca7a069e5
Mohammad Afzali <afzaliwp@gmail.com>
2025-06-11 12:33:50 +0330

    remove extra files

--- template-parts/research/content.php
+++
@@ -1,8 +0,0 @@
-<div class="container relative mt-16 md:mt-23 mb-20">
-	<div class="w-[930px] mx-auto flex flex-col gap-4 md:gap-6">
-		<h2 class="h2 font-bold text-black-500">PUBLICATIONS</h2>
-		<div class="body1 font-normal text-black-500">
-			<?php the_content(); ?>
-		</div>
-	</div>
-</div>
